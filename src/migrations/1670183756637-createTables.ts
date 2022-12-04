cbd9b05192a0a89ea154d2fc42b98931af251daf
Debora Correa <deboragomescorr@gmail.com>
2022-12-04 18:33:54 -0300

    feat: migration creates tables

---
+++ src/migrations/1670183756637-createTables.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class createTables1670183756637 implements MigrationInterface {
+    name = 'createTables1670183756637'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL, "fullName" character varying(100) NOT NULL, "username" character varying(20) NOT NULL, "password" character varying NOT NULL, "email" character varying(60) NOT NULL, "number" character varying(15) NOT NULL, "registrationDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_aa517eb9a603fc94080a45ae983" UNIQUE ("number"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "contacts" ("id" uuid NOT NULL, "fullName" character varying(80) NOT NULL, "email" character varying(60) NOT NULL, "number" character varying(15) NOT NULL, "ownerId" uuid NOT NULL, CONSTRAINT "PK_b99cd40cfd66a99f1571f4f72e6" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "contacts" ADD CONSTRAINT "FK_270a85b7f2d4b6821dc7642e6a8" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "contacts" DROP CONSTRAINT "FK_270a85b7f2d4b6821dc7642e6a8"`);
+        await queryRunner.query(`DROP TABLE "contacts"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
