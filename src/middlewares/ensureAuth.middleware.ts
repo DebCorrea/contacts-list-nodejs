6c65c2dc891c670ef66abfaea1e4a5251a78da40
Debora Correa <deboragomescorr@gmail.com>
2022-12-04 20:14:52 -0300

    feat: middleware ensures jwt authentication

---
+++ src/middlewares/ensureAuth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+
+const ensureAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token = req.headers.authorization;
+
+  if (!token) {
+    throw new AppError("Missing authorization header", 401);
+  }
+
+  token = token.split(" ")[1];
+
+  jwt.verify(
+    token,
+    process.env.SECRET_KEY as string,
+    (error: any, decoded: any) => {
+      if (error) {
+        return res.status(401).json({
+          message: "Invalid token",
+        });
+      }
+
+      req.userId = decoded.id;
+
+      next();
+    }
+  );
+};
+
+export { ensureAuth };
