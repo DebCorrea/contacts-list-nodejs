5ff565dd77655e4222677eec6e7716669afabdff
Debora Correa <deboragomescorr@gmail.com>
2022-12-11 16:32:04 -0300

    feat: route, controller, service for contact deletion

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -8,6 +8,7 @@ import { listContactsController } from "../controllers/contacts/listContacts.con
 import { isContactOwner } from "../middlewares/isContactOwner.middleware";
 import { listContactController } from "../controllers/contacts/listContact.controller";
 import { updateContactController } from "../controllers/contacts/updateContact.controller";
+import { deleteContactController } from "../controllers/contacts/deleteContact.controller";
 
 const routes = Router();
 
@@ -21,6 +22,7 @@ const contactRoutes = () => {
   routes.get("/", ensureAuth, listContactsController);
   routes.get("/:id", ensureAuth, isContactOwner, listContactController);
   routes.patch("/:id", ensureAuth, isContactOwner, updateContactController);
+  routes.delete("/:id", ensureAuth, isContactOwner, deleteContactController);
 
   return routes;
 };
