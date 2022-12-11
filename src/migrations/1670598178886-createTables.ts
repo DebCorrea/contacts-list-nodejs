6902f8c4920c55181f6c62b155853167f5da7b10
Debora Correa <deboragomescorr@gmail.com>
2022-12-11 11:44:41 -0300

    feat: new migrations generated

--- src/migrations/1670598178886-createTables.ts
+++ src/migrations/1670598178886-createTables.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createTables1670183756637 implements MigrationInterface {
-    name = 'createTables1670183756637'
+export class createTables1670598178886 implements MigrationInterface {
+    name = 'createTables1670598178886'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL, "fullName" character varying(100) NOT NULL, "username" character varying(20) NOT NULL, "password" character varying NOT NULL, "email" character varying(60) NOT NULL, "number" character varying(15) NOT NULL, "registrationDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_aa517eb9a603fc94080a45ae983" UNIQUE ("number"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
